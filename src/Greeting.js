745808352b9e6ec8182a6cd8b9948b0eb94582f2
dimpyvaidya <55465452+dimpyvaidya@users.noreply.github.com>
2020-04-26 23:09:43 +0530

    formating and style wrong import

---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from 'img/logo.jpg';
+import "./../../css/style.css";
+
+const Greeting = () => {
+  return (
+    <>
+      <header class="branding">
+        <h1> Lets 's Connect</h1>
+      </header>
+    </>
+  );
+};
+
+export default Greeting;
