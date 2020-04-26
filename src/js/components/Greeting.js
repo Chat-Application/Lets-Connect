2f790e03a2c05cef95e6f90eae3bffd40cd66326
dimpyvaidya <55465452+dimpyvaidya@users.noreply.github.com>
2020-04-26 23:51:48 +0530

    changes

--- src/js/components/Greeting.js
+++ src/js/components/Greeting.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 // import logo from 'img/logo.jpg';
-import 'css/index.css'
+import "./../../css/style.css";
 
 const Greeting = () => {
-        return ( <
-            >
-            <
-            header class = "branding" >
-            <
-            h1 > Lets 's Connect</h1> < /
-            header > <
-            />)
-        }
+  return (
+    <>
+      <header class="branding">
+        <h1> Lets 's Connect</h1>
+      </header>
+    </>
+  );
+};
 
-        export default Greeting;
+export default Greeting;
--- src/js/components/Profile.js
+++ src/js/components/Profile.js
@@ -1,345 +1,197 @@
-import React from 'react';
-import bhawan from 'img/bhawan.jpg';
-import dimpy from 'img/dimpy.jpg';
+import React from "react";
+import bhawan from "img/bhawan.jpg";
+import dimpy from "img/dimpy.jpg";
 
-import '/css/style.css'
-
+// import "/css/style.css";
 
 const Profile = () => {
+  return (
+    <>
+      <aside className="others open" id="others">
+        <div className="panel">
+          <h2> My Contacts </h2>
+          <div className="people-list" id="people-list">
+            <div className="searchbox searchnav search-container">
+              <input type="text" placeholder="Search.." name="search" />
+              <i className="fa fa-search"> </i>
+            </div>
+            <ul className="list">
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Bhawan Sidhu </div>
+                  <div className="status">
+                    <i className="fa fa-circle online"> </i> online
+                  </div>
+                </div>
+              </li>
 
-    return ( <
-        >
-        <
-        aside class = "others open"
-        id = "others" >
-        <
-        div class = "panel" >
-        <
-        h2 > My Contacts < /h2> <
-        div class = "people-list"
-        id = "people-list" >
-        <
-        div class = "searchbox searchnav search-container" >
-        <
-        input type = "text"
-        placeholder = "Search.."
-        name = "search" >
-        <
-        i class = "fa fa-search" > < /i> < /
-        div > <
-        ul class = "list" >
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Bhawan Sidhu < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle online" > < /i> online < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={dimpy} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Dimpy </div>
+                  <div className="status">
+                    <i className="fa fa-circle online"> </i> online
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { dimpy }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Dimpy < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle online" > < /i> online < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Aman </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> 3 hours ago
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Aman < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> 3 hours ago < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Shreya </div>
+                  <div className="status">
+                    <i className="fa fa-circle online"> </i> online
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Shreya < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle online" > < /i> online < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Joseph </div>
+                  <div className="status">
+                    <i className="fa fa-circle online"> </i> online
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Joseph < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle online" > < /i> online < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Kunal </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> left 30 mins ago
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Kunal < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> left 30 mins ago < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Inna </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> left 11 hours ago
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Inna < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> left 11 hours ago < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Bijay </div>
+                  <div className="status">
+                    <i className="fa fa-circle online"> </i> online
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Bijay < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle online" > < /i> online < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Manpreet </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> offline yesterday
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Manpreet < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> offline yesterday < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Avleen </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> offline since
+                    March 12
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Avleen < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> offline since March 12 < /
-        div > <
-        /div> < /
-        li >
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Hardeep </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> offline Yeasterday
+                  </div>
+                </div>
+              </li>
 
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Hardeep < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> offline Yeasterday < /
-        div > <
-        /div> < /
-        li >
-
-        <
-        li class = "clearfix" >
-        <
-        img src = { bhawan }
-        alt = "avatar" / >
-        <
-        div class = "about" >
-        <
-        div class = "name" > Mandeep < /div> <
-        div class = "status" >
-        <
-        i class = "fa fa-circle offline" > < /i> offline 2 hours ago < /
-        div > <
-        /div> < /
-        li >
-
-
-
-
-        <
-        /ul> < /
-        div >
-
-        <
-        /div> <
-        button class = "toggle-others"
-        id = "btnOthers" > < span > chats < /span></button >
-        <
-        /aside> <
-        main class = "conversation"
-        id = "conversation" >
-        <
-        div >
-        <
-        div class = "chat-msg" >
-        <
-        h2 > Chat Messages < /h2> < /
-        div >
-
-        <
-        div class = "container" >
-        <
-        img src = { bhawan }
-        alt = "Avatar"
-        style = "width:100%;" >
-        <
-        p > Hello.How are you today ? < /p> <
-        span class = "time-right" > 11 : 00 AM < /span> < /
-        div >
-
-        <
-        div class = "container darker" >
-        <
-        img src = { dimpy }
-        alt = "Avatar"
-        class = "right"
-        style = "width:100%;" >
-        <
-        p > Hey!I 'm fine. Thanks for asking!</p> <
-        span class = "time-left" > 11: 01 AM < /span> < /
-        div >
-
-        <
-        div class = "container" >
-        <
-        img src = { bhawan }
-        alt = "Avatar"
-        style = "width:100%;" >
-        <
-        p > So, are you done with your work today ? < /p> <
-        span class = "time-right" > 11 : 02 AM < /span> < /
-        div >
-
-        <
-        div class = "container darker" >
-        <
-        img src = { dimpy }
-        alt = "Avatar"
-        class = "right"
-        style = "width:100%;" >
-        <
-        p > Nah, not yet. < /p> <
-        span class = "time-left" > 11: 05 AM < /span> < /
-        div > <
-        div class = "container" >
-        <
-        img src = { bhawan }
-        alt = "Avatar"
-        style = "width:100%;" >
-        <
-        p > Why ? < /p> <
-        span class = "time-right" > 11 : 00 AM < /span> < /
-        div >
-
-        <
-        div class = "container darker" >
-        <
-        img src = { dimpy }
-        alt = "Avatar"
-        class = "right"
-        style = "width:100%;" >
-        <
-        p > Actually I was feeling unwell, so slept
-        for a
-        while ! < /p> <
-        span class = "time-left" > 11: 01 AM < /span> < /
-        div >
-
-        <
-        div class = "container" >
-        <
-        img src = { bhawan }
-        alt = "Avatar"
-        style = "width:100%;" >
-        <
-        p > Ok, even I went
-        for shopping today. < /p> <
-        span class = "time-right" > 11: 02 AM < /span> < /
-        div >
-
-        <
-        div class = "container darker" >
-        <
-        img src = { dimpy }
-        alt = "Avatar"
-        class = "right"
-        style = "width:100%;" >
-        <
-        p > Great, I 'll update you once I'
-        m done < /p> <
-        span class = "time-left" > 11: 05 AM < /span> < /
-        div > <
-        /div> < /
-        main > <
-        /div> < / >
-    )
-}
+              <li className="clearfix">
+                <img src={bhawan} alt="avatar" />
+                <div className="about">
+                  <div className="name"> Mandeep </div>
+                  <div className="status">
+                    <i className="fa fa-circle offline"> </i> offline 2 hours
+                    ago
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button className="toggle-others" id="btnOthers">
+          <span> chats </span>
+        </button>
+      </aside>
+      <main className="conversation" id="conversation">
+        <div>
+          <div className="chat-msg">
+            <h2> Chat Messages </h2>
+          </div>
+          <div className="container">
+            <img src={bhawan} alt="Avatar" />
+            <p> Hello.How are you today ? </p>
+            <span className="time-right"> 11 : 00 AM </span>
+          </div>
+          <div className="container darker">
+            <img src={dimpy} alt="Avatar" className="right" />
+            <p> Hey!I 'm fine. Thanks for asking!</p>
+            <span className="time-left"> 11: 01 AM </span>
+          </div>
+          <div className="container">
+            <img src={bhawan} alt="Avatar" />
+            <p> So, are you done with your work today ? </p>
+            <span className="time-right"> 11 : 02 AM </span>
+          </div>
+          <div className="container darker">
+            <img src={dimpy} alt="Avatar" className="right" />
+            <p> Nah, not yet. </p>{" "}
+            <span className="time-left"> 11: 05 AM </span>
+          </div>
+          <div className="container">
+            <img src={bhawan} alt="Avatar" />
+            <p> Why ? </p> <span className="time-right"> 11 : 00 AM </span>
+          </div>
+          <div className="container darker">
+            <img src={dimpy} alt="Avatar" className="right" />
+            <p> Actually I was feeling unwell, so slept for a while ! </p>
+            <span className="time-left"> 11: 01 AM </span>
+          </div>
+          <div className="container">
+            <img src={bhawan} alt="Avatar" />
+            <p> Ok, even I went for shopping today. </p>
+            <span className="time-right"> 11: 02 AM </span>
+          </div>
+          <div className="container darker">
+            <img src={dimpy} alt="Avatar" className="right" />
+            <p> Great, I 'll update you once I' m done </p>
+            <span className="time-left"> 11: 05 AM </span>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Profile;
